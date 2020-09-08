c7758a6bd645d6db63f6479e21e0d9d157b5b030
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-08 07:47:03 +0000

    Deploy loogar/my-portfolio to github.com/loogar/my-portfolio.git:gh-pages

---
+++ precache-manifest.8e0c4803276ab58c18a4ca8aa919f6c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cb89317e9e335d17758",
+    "url": "/static/css/main.61ccc7e5.chunk.css"
+  },
+  {
+    "revision": "3cb89317e9e335d17758",
+    "url": "/static/js/main.3cb89317.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "facf1f2d2bae118f1732",
+    "url": "/static/js/2.facf1f2d.chunk.js"
+  },
+  {
+    "revision": "32fd6e78003a04a285ebae265ee335d0",
+    "url": "/index.html"
+  }
+];
